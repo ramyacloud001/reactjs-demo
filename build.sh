3e86046dd0296963d2d390f6abd084b6988ab36d
ramyacloud001 <126871924+ramyacloud001@users.noreply.github.com>
2023-06-22 20:09:59 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,4 @@ IMAGE_NAME="ramyacloud001/dev"
 IMAGE_TAG=$(git rev-parse --short HEAD) # first 7 characters of the current commit hash
 
 echo "Building Docker image ${IMAGE_NAME}:${IMAGE_TAG}, and tagging as latest"
-docker build . -t "${IMAGE_NAME}:${IMAGE_TAG}" 
-docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:latest"
+docker build -f Dockerfile -t ${IMAGE_NAME}:${IMAGE_TAG} .
