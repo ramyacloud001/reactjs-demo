af54e96b500a07fc6e653ec07f447b49e8cd0b99
ramyacloud001 <126871924+ramyacloud001@users.noreply.github.com>
2023-06-22 20:16:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 
 # Stop script on first error
 set -e
-
+DOCKER_USERNAME="ramyacloud001"
+DOCKER_PASSWORD="Ram@jamesbond007"
+echo "Authenticating and pushing image to Docker Hub"
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 IMAGE_NAME="ramyacloud001/dev"
 IMAGE_TAG=$(git rev-parse --short HEAD) # first 7 characters of the current commit hash
 
