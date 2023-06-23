eee5b1d3c1fc18f411e95a7b8a9fa1a0e37bbdb8
ramyacloud001 <126871924+ramyacloud001@users.noreply.github.com>
2023-06-23 11:25:50 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-#DOCKER_USERNAME="ramyacloud001"
-#DOCKER_PASSWORD="Ram@jamesbond007"
-#echo "Authenticating and pushing image to Docker Hub"
-#echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-#docker push ${IMAGE_NAME}:${IMAGE_TAG}
-docker push "${IMAGE_NAME}:latest"
+DOCKER_USERNAME="ramyacloud001"
+DOCKER_PASSWORD="Ram@jamesbond007"
+echo "Authenticating and pushing image to Docker Hub"
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+docker push ${IMAGE_NAME}:${IMAGE_TAG}
+docker push ${IMAGE_NAME}:latest
 echo "Successfully deployed"
